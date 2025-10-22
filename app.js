303dd0691843c8a80968559faa1dc8ca8b35b48f
Aria <arya_math@yahoo.com>
2025-10-21 20:13:01 -0400

    "new changes"

--- app.js
+++ app.js
@@ -52,14 +52,6 @@ app.use(fileUpload())
 
 
 
-app.get('/' , (req , res) => {
-  res.send('e-commerce api')
-})
-
-app.get('/api/v1' , (req , res) => {
-  console.log(req.signedCookies);
-  res.send('e-commerce api')
-})
 
 app.use('/api/v1/auth' , authRouter)
 app.use('/api/v1/users' , userRouter)
